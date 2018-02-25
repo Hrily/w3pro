c1906eed612ef8f965ad19896a1a977467324a79
Enrico Caputo <enricocaputo.1972@gmail.com>
2018-02-25 22:58:35 +0100

    Update bundle.js

--- js/bundle.js
+++ js/bundle.js
@@ -1,14 +1,15 @@
 // Script for Sidebar, Tabs, Accordions, Progress bars and slideshows
 // ------------------------------------------------------------------
-// Side navigation
-function w3_open() {
-    document.getElementById("mySidebar").style.display = "block";
-    document.getElementById("myOverlay").style.display = "block";
-}
-function w3_close() {
-    document.getElementById("mySidebar").style.display = "none";
-    document.getElementById("myOverlay").style.display = "none";
-}
+// Nav - Used to toggle the menu on small screens when clicking on the menu button
+  function myFunction() {
+      var x = document.getElementById("navDemo");
+      if (x.className.indexOf("w3-show") == -1) {
+          x.className += " w3-show";
+      } else {
+          x.className = x.className.replace(" w3-show", "");
+      }
+  }
+
 
 // Tabs
 function openCity(evt, cityName) {
